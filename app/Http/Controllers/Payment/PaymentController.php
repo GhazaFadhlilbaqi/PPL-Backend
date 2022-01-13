09f54e85697147ed9bf72dddcdb26a46ab7a624b
David Mahbubi <ulrichdavid0370@gmail.com>
2022-01-13 12:15:58 +0700

    Improved payment system validation

--- app/Http/Controllers/Payment/PaymentController.php
+++ app/Http/Controllers/Payment/PaymentController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Midtrans\Config;
 use App\Http\Controllers\Midtrans\Snap;
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 use Vinkla\Hashids\Facades\Hashids;
 
@@ -37,21 +38,43 @@ class PaymentController extends Controller
         $orderId = $this->generateOrderId();
         $projectId = Hashids::decode($request->project_id)[0];
 
-        $pendingOrder = Auth::user()->order()->where('project_id', $projectId)->where('status', 'pending')->first();
+        /**
+         * Payment verification step
+         *
+         * 1. Check if there's any pending or waiting_for_payment order
+         * 2. Check for midtrans status
+         * 3. Re use order if it's not expired (status from midtrans)
+         */
 
-        if ($pendingOrder) {
-            return response()->json([
-                'status' => 'fail',
-                'message' => 'Masih terdapat pembayaran yang pending ! mohon tunggu hingga anda mendapatkan email konfirmasi pembayaran'
-            ], 400);
+        $order = Auth::user()->order()->where('project_id', $projectId)->orderBy('created_at', 'DESC')->take(1)->first();
+
+        if ($order && in_array($order->status, ['pending', 'waiting_for_payment'])) {
+
+            // Check midtrans status
+            $midtransStatus = $this->checkStatusOrder($order->order_id);
+            $midtransStatusCode = $midtransStatus['status_code'];
+
+            // If it's expired or it's completed (but marked as pending or expired)
+            if ($midtransStatusCode == '407' || $midtransStatusCode == '200') {
+                $order->status = $midtransStatusCode == '407' ? 'expired' : 'completed';
+                $order->save();
+                $order = $this->setOrder($orderId, $customer, Hashids::decode($request->project_id)[0], self::productPrice);
+            }
+
         }
+
+        // Check if there's a pending order
+        // $pendingOrder = Auth::user()->order()->where('project_id', $projectId)->where('status', 'pending')->first();
+
+        // if ($pendingOrder) {
+        //     return response()->json([
+        //         'status' => 'fail',
+        //         'message' => 'Masih terdapat pembayaran yang pending ! mohon tunggu hingga anda mendapatkan email konfirmasi pembayaran'
+        //     ], 400);
+        // }
 
         // Check if the order already created or not
-        $order = Auth::user()->order()->where('project_id', $projectId)->where('status', 'waiting_for_payment')->first();
-
-        if (!$order) {
-            $order = $this->setOrder($orderId, $customer, Hashids::decode($request->project_id)[0], self::productPrice);
-        }
+        // $order = Auth::user()->order()->where('project_id', $projectId)->where('status', 'waiting_for_payment')->first();
 
         $buyedItems[] = [
             'id' => uniqid(),
@@ -137,5 +160,17 @@ class PaymentController extends Controller
     private function generateOrderId()
     {
         return strtoupper(Str::random(16));
+    }
+
+    private function checkStatusOrder($orderId)
+    {
+
+        $midtransApiUrl = env('MIDTRANS_MODE') == 'production' ? 'https://api.midtrans.com' : 'https://api.sandbox.midtrans.com';
+
+        $statusRequest = Http::withHeaders([
+            'Authorization' => 'Basic ' . base64_encode(env('MIDTRANS_SERVER_KEY') . ':'),
+        ])->get($midtransApiUrl . '/v2' . '/' . $orderId . '/status');
+
+        return $statusRequest->json();
     }
 }
