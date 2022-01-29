cdd1a459fcf6f70daf90888ad384edfa1cc394d6
David Mahbubi <ulrichdavid0370@gmail.com>
2022-01-29 12:52:16 +0700

    Updated Verify Mail Background to follow company's color accent

--- resources/views/mails/auth/email-verifiation.blade.php
+++ resources/views/mails/auth/email-verifiation.blade.php
@@ -10,7 +10,7 @@
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
     <!-- LOGO -->
     <tr>
-        <td bgcolor="#FFA73B" align="center">
+        <td bgcolor="#00365a" align="center">
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                 <tr>
                     <td align="center" valign="top" style="padding: 40px 10px 40px 10px;"> </td>
@@ -19,7 +19,7 @@
         </td>
     </tr>
     <tr>
-        <td bgcolor="#FFA73B" align="center" style="padding: 0px 10px 0px 10px;">
+        <td bgcolor="#00365a" align="center" style="padding: 0px 10px 0px 10px;">
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                 <tr>
                     <td bgcolor="#ffffff" align="center" valign="top"
@@ -47,8 +47,8 @@
                                 <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
                                     <table border="0" cellspacing="0" cellpadding="0">
                                         <tr>
-                                            <td align="center" style="border-radius: 3px;" bgcolor="#FFA73B">
-                                                <a href="{{ route('register.confirm_email', ['token' => $token]) }}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;">Confirm Account</a>
+                                            <td align="center" style="border-radius: 3px;" bgcolor="#00365a">
+                                                <a href="{{ route('register.confirm_email', ['token' => $token]) }}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #00365a; display: inline-block;">Confirm Account</a>
                                             </td>
                                         </tr>
                                     </table>
@@ -59,57 +59,15 @@
                 </tr> <!-- COPY -->
                 <tr>
                     <td bgcolor="#ffffff" align="left"
-                        style="padding: 0px 30px 0px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                        <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:
-                        </p>
-                    </td>
-                </tr> <!-- COPY -->
-                <tr>
-                    <td bgcolor="#ffffff" align="left"
-                        style="padding: 20px 30px 20px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                        <p style="margin: 0;"><a href="#" target="_blank"
-                                style="color: #FFA73B;">https://bit.li.utlddssdstueincx</a></p>
-                    </td>
-                </tr>
-                <tr>
-                    <td bgcolor="#ffffff" align="left"
                         style="padding: 0px 30px 20px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                        <p style="margin: 0;">If you have any questions, just reply to this email—we're always happy to
-                            help out.</p>
+                        <p style="margin: 0;">If you have any questions, just reply to this email : admin@rencanakan.id </p>
+                        <p>We're always happy to help out.</p>
                     </td>
                 </tr>
                 <tr>
                     <td bgcolor="#ffffff" align="left"
                         style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                        <p style="margin: 0;">Cheers,<br>BBB Team</p>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <td bgcolor="#f4f4f4" align="center" style="padding: 30px 10px 0px 10px;">
-            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                <tr>
-                    <td bgcolor="#FFECD1" align="center"
-                        style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                        <h2 style="font-size: 20px; font-weight: 400; color: #111111; margin: 0;">Need more help?</h2>
-                        <p style="margin: 0;"><a href="#" target="_blank" style="color: #FFA73B;">We&rsquo;re here to
-                                help you out</a></p>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
-            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                <tr>
-                    <td bgcolor="#f4f4f4" align="left"
-                        style="padding: 0px 30px 30px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;">
-                        <br>
-                        <p style="margin: 0;">If these emails get annoying, please feel free to <a href="#"
-                                target="_blank" style="color: #111111; font-weight: 700;">unsubscribe</a>.</p>
+                        <p style="margin: 0;">Cheers,<br>Rencanakan.id Team</p>
                     </td>
                 </tr>
             </table>
