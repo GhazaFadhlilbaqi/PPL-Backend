973e7fd5da77856af27fc5304b4d7527d13aabfd
Filbert Hartawan <filberthtwn@gmail.com>
2025-01-22 00:11:17 +0700

    remove unused code

--- resources/js/generate-pdf.js
+++ resources/js/generate-pdf.js
@@ -4,9 +4,6 @@ async function generatePdf() {
     const browser = await puppeteer.launch({
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        executablePath: puppeteer.executablePath({
-            cacheDirectory: process.env.PUPPETEER_EXECUTABLE_PATH || '',
-        }),
     });
     const page = await browser.newPage();
     const html = process.argv[2];
