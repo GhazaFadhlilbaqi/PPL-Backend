d7f9379643710467e4633fa093857ee8f904bcb5
Filbert Hartawan <filberthtwn@gmail.com>
2025-01-21 23:34:32 +0700

    add temporary try catch

--- resources/js/generate-pdf.js
+++ resources/js/generate-pdf.js
@@ -1,22 +1,27 @@
 const puppeteer = require("puppeteer");
 
 async function generatePdf() {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-    const page = await browser.newPage();
-    const html = process.argv[2];
+    try {
 
-    await page.setContent(html);
-    await page.waitForSelector("canvas");
-    await page.pdf({
-        path: process.argv[3],
-        format: "A4",
-        printBackground: true,
-        margin: { top: '16px', left: '32px' }
-    });
-    await browser.close();
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const page = await browser.newPage();
+        const html = process.argv[2];
+    
+        await page.setContent(html);
+        await page.waitForSelector("canvas");
+        await page.pdf({
+            path: process.argv[3],
+            format: "A4",
+            printBackground: true,
+            margin: { top: '16px', left: '32px' }
+        });
+        await browser.close();
+    } catch (error) {
+        console.error('Error running Puppeteer:', error);
+      }
 }
 
 generatePdf();
